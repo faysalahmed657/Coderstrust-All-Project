e18f3b9aa36527cab7404ec585323a09a3db523a
ahmedmdfaysal223@gmail.com <ahmedmdfaysal223@gmail.com>
2021-09-27 14:51:00 +0600

    First Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <meta charset="UTF-8">
+       <meta name="description" content="myphp">
+       <meta name="keywords" content="myphp, html, css">
+        <style>
+
+</style>
+        <title>
+            <?php echo "My name is Faysal" ?>
+        </title>
+    </head>
+    <body>
+        <h1><?php?</h1>
+    </body>
+</html>
